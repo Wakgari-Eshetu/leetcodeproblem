d40ac079dd91905a0ca17d785e3762fe4f706111
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2025-11-19 17:07:41 +0300

    Time: 0 ms (100%), Space: 17.9 MB (37.74%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.py
@@ -0,0 +1,14 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        nums = list(map(str,nums))
+        def compare(x,y):
+            if x+y>y+x:
+                return -1
+            return 1
+        nums.sort(key=cmp_to_key(compare))
+        result = ''.join(nums)
+        if result[0]=="0":
+            return '0'
+        return result
+
+        
