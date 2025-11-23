ef22743447f76c8b00fc1711c51a19ea3e494384
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2025-11-24 00:19:15 +0300

    Time: 3 ms (37.28%), Space: 17.8 MB (83.17%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,17 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack =[]
+        segment = path.split('/')
+        for c in segment:
+            if c=='' or c=='.':
+                continue
+            elif c=='..':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(c)
+        return '/'+'/'.join(stack)
+
+            
+
+        
