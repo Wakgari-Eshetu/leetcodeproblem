a729be8a67e01db8f106a608c2057def9f2f3381
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2026-02-11 14:04:27 +0300

    Time: 2 ms (82.27%), Space: 19.2 MB (57.88%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,27 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        result =[]
+        hash_map  = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I")
+        ]
+        for value , symbol in hash_map:
+            while num >= value:
+                result.append(symbol)
+                num -= value
+
+    
+        return "".join(result )
+
+        
