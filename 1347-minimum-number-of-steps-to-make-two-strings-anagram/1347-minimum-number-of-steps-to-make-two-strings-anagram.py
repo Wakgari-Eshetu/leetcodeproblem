c1e549b5d62e1791b83a746562fe9efbeac8cfff
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2026-02-12 14:23:32 +0300

    Time: 63 ms (79.49%), Space: 19.7 MB (55.52%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -1,7 +1,14 @@
 class Solution:
     def minSteps(self, s: str, t: str) -> int:
-        map1 = Counter(s)
-        map2 = Counter(t)
+        count1 = Counter(t)
+        count2 = Counter(s)
+        
+        result = 0
+        for char in count1:
+            if count1[char] > count2[char]:
+                result += count1[char] - count2[char]
+        
+        return result 
 
-        return sum((map1-map2).values())
+
         
