01cf7c32e220c19258df21c92720f44bfb7387a8
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2026-02-12 10:56:43 +0300

    Time: 3 ms (90.37%), Space: 23 MB (42%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,7 +1,11 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
         count = Counter(nums)
-        result = [elem for elem ,f in count.most_common(k)] 
-        return result
-                
+        
+        
+        return [value for value,index in count.most_common(k)]
+        
+
+
+        
+        
