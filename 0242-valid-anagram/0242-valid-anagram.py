05b6d61cf8e4221ae28844299b7cc09e9f56343f
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2026-02-12 11:06:15 +0300

    Time: 7 ms (90.75%), Space: 19.5 MB (28.51%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False 
-        return sorted(s) == sorted(t)
+        count1 = Counter(s)
+        count2 = Counter(t)
+
+        return count1 == count2
