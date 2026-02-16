caf88faabf91867feadc39971e3bb294b691e3f6
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2026-02-16 10:42:03 +0300

    Time: 37 ms (84.66%), Space: 19.3 MB (22.24%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,11 +1,7 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         bit = list(format(n, '032b'))
-        left,right  = 0 ,len(bit)-1
-        while left<right:
-            bit[left],bit[right]=bit[right],bit[left]
-            left+=1
-            right -=1
-        ans = int(''.join(bit),2)
+        rev = bit[::-1]
+        ans = int(''.join(rev),2)
         return ans
         
