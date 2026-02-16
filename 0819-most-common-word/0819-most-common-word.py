31cddedbe74d6ad8587c8485f9535665cf633f7c
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2026-02-16 18:50:32 +0300

    Time: 0 ms (100%), Space: 19.7 MB (15.19%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.py
+++ 0819-most-common-word/0819-most-common-word.py
@@ -1,5 +1,6 @@
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        banned = set(banned)
         count = Counter(re.findall(r"[a-z]+", paragraph.lower()))
         for char , value in count.most_common():
             if char not in banned:
