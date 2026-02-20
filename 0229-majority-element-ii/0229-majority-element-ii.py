ca1324fdd1d9b21a9c1530a96bc399c30030ad28
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2026-02-20 17:29:05 +0300

    Time: 5 ms (75.55%), Space: 23.6 MB (16%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,5 +1,5 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
         count = Counter(nums)
-        n = len(nums)//3
-        return [num for num , freq in count.items() if freq > n]
+        return  [x  for x in count if count[x] > len(nums)/3]
+        
