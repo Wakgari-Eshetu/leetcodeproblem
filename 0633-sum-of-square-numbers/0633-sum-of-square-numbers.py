449333b013aab35d48c99dc6b0a15247ab9c73c8
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2026-02-24 17:38:42 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -0,0 +1,18 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        left , right = 0 , c
+
+        
+        while left <= right:
+            if left*left + right *right ==  c :
+                return True 
+            elif left*left + right *right >  c:
+                right -= 1
+            else:
+                left += 1
+        
+        return False 
+
+
+        
+        
