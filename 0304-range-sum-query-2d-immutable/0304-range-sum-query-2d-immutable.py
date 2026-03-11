4f6217fa5180fcc5722274c70146094d01ff9017
Wakgari-Eshetu <lelikenakoo663@gmail.com>
2026-03-11 18:44:24 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -1,18 +1,16 @@
 class NumMatrix:
+
     def __init__(self, matrix: List[List[int]]):
-        
         rows, cols = len(matrix), len(matrix[0])
-        self.prefix_sum = [[0] * (cols + 1) for _ in range(rows + 1)]
-      
-        for i, row in enumerate(matrix):
-            for j, value in enumerate(row):
-                self.prefix_sum[i + 1][j + 1] = (
-                    self.prefix_sum[i][j + 1] + self.prefix_sum[i + 1][j] - self.prefix_sum[i][j] +  value                            
-                )
+        self._sum = [[0 for _ in range(cols + 1)] for _ in range(rows)]
+        for i in range(rows):
+            cur_sum = 0
+            for j in range(cols):
+                cur_sum += matrix[i][j]
+                self._sum[i][j+1] = cur_sum
+
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        return sum([row[col2+1] - row[col1] for row in self._sum[row1:row2+1]])
         
-        return (
-            self.prefix_sum[row2 + 1][col2 + 1] - self.prefix_sum[row2 + 1][col1] - self.prefix_sum[row1][col2 + 1] +        
-            self.prefix_sum[row1][col1]              
-        )
+        
